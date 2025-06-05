0aeb41161ab12de9be9e5f0dd21d35faa2cedf5d
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-05 19:29:31 -0400

    Add files via upload

--- productos.js
+++ productos.js
@@ -1,3 +1,4 @@
+
 const CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS4jvq-eB9Fn1bZQjdtiboCyn-0sGswn24iWNdJsWqw0MCz0AOhNoId6BKw8ZLFSg/pub?output=csv';
 
 fetch(CSV_URL)
@@ -18,7 +19,7 @@ fetch(CSV_URL)
             <p><strong>Código:</strong> ${codigo}</p>
             <p><strong>Marca:</strong> ${marca}</p>
             <p><strong>Procedencia:</strong> ${procedencia}</p>
-            <p><strong>Precio:</strong> Bs ${parseFloat(precioUSD).toFixed(2)}</p>
+            <p><strong>Precio:</strong> Bs ${(parseFloat(precioUSD) * 6.96).toFixed(2)}</p>
             <p class="nota">*Precio personalizado según tipo de cliente</p>
           </div>
           <img class="qr" src="${qr || 'https://via.placeholder.com/100'}" alt="QR">
@@ -29,6 +30,5 @@ fetch(CSV_URL)
     });
   })
   .catch(error => {
-    console.error('Error al cargar el CSV:', error);
     document.getElementById("contenedor-productos").innerHTML = "<p>Error al cargar los productos.</p>";
   });
