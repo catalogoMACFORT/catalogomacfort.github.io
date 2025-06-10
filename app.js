14a896add67a456a8f824076b4b9c83fc84aa346
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-10 15:03:41 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contenido = document.getElementById('contenido');
+    contenido.innerHTML = '<h2>Seleccione su tipo de cliente para ver precios.</h2>';
+    // Simulación básica
+});
