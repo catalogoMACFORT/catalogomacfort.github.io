7eccd49cc794f30ab475152e8bbd1c124354ae60
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-10 12:54:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-document.querySelector('button:last-of-type').addEventListener('click', () => {
-    document.getElementById('registro').style.display = 'none';
-    document.getElementById('catalogo').style.display = 'block';
+document.addEventListener("DOMContentLoaded", () => {
+    const productList = document.getElementById("product-list");
+    productList.innerHTML = "<p>Cargando productos desde Google Sheets...</p>";
 });
-    
