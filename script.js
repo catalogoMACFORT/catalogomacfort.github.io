628918397a7a26a2efe8b90d0b0cc9b8a8144ad1
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-10 15:06:16 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById('clienteTipo').addEventListener('change', function() {
-  let cliente = this.value;
-  if(cliente) {
-    window.location.href = "https://wa.me/59168099278?text=Solicito%20PIN%20para%20" + cliente;
-  }
-});
