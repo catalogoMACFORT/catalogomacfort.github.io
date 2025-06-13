413d9e1b1ff492002cf524778668987ee8bca45c
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-13 19:19:44 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import ClientPricingCalculator from './components/ClientPricingCalculator';
+
+export default function App() {
+  return (
+    <div className="p-4">
+      <ClientPricingCalculator />
+    </div>
+  );
+}
