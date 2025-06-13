48b2dc1e9823b05014af61734112720fef0a9065
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-13 11:34:17 -0400

    Delete idioma.js

--- idioma.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    const selector = document.createElement("select");
-    selector.innerHTML = `
-        <option value="es">Español</option>
-        <option value="en">English</option>
-    `;
-    document.body.insertBefore(selector, document.body.firstChild);
-});
