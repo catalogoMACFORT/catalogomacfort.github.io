bd9529aabb062ede675cdcfd832d36e6b8945531
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-13 09:19:14 -0400

    Delete whatsapp.js

--- whatsapp.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.createElement("a");
-    btn.href = "https://wa.me/59168099278";
-    btn.innerText = "Contáctanos por WhatsApp";
-    btn.className = "whatsapp-button";
-    document.body.appendChild(btn);
-});
