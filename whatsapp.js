d1c7439fdc2f499273b861639953ee4176b25484
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-13 08:59:05 -0400

    Add files via upload

---
+++ whatsapp.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.createElement("a");
+    btn.href = "https://wa.me/59168099278";
+    btn.innerText = "Contáctanos por WhatsApp";
+    btn.className = "whatsapp-button";
+    document.body.appendChild(btn);
+});
