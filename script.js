a5f902c023d457c8baf4d1772b1bcd40d0cd8d33
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-13 19:56:05 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function verCatalogo() {
+    const catalogo = document.getElementById('catalogo');
+    catalogo.innerHTML = `
+        <div>
+            <h2>Escalera Extensible 2x10</h2>
+            <p>Precio: 2200 Bs | $135</p>
+        </div>
+        <div>
+            <h2>Escalera Extensible 2x12</h2>
+            <p>Precio: 2500 Bs | $153</p>
+        </div>
+    `;
+}
