01e0a3fd6181545d9000a882ae64cd5e6667a6f3
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-13 09:44:26 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
   fetch('productos.json')
     .then(res => res.json())
     .then(data => mostrarProductos(data));
+  mostrarCarrito();
 });
 
 function mostrarProductos(productos) {
   const tipo = localStorage.getItem("tipoUsuario") || "cliente";
   const contenedor = document.getElementById("productos");
-  contenedor.innerHTML = productos.map(prod => {
+  contenedor.innerHTML = productos.map((prod, index) => {
     let precio = prod.precio_final;
     if (tipo === "distribuidor") precio = prod.precio_distribuidor;
     if (tipo === "mayorista") precio = prod.precio_mayorista;
@@ -16,7 +17,20 @@ function mostrarProductos(productos) {
       <div class="producto">
         <h3>${prod.nombre}</h3>
         <p>Precio (${tipo}): ${precio} Bs</p>
+        <button onclick="agregarAlCarrito(${index})">Agregar al carrito</button>
       </div>
     `;
   }).join('');
+}
+
+function agregarAlCarrito(index) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  carrito.push(index);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  mostrarCarrito();
+}
+
+function mostrarCarrito() {
+  const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+  document.getElementById("carrito").innerText = `🛒 Carrito (${carrito.length})`;
 }
