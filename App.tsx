c6cae30e89e9583024f00241aeb0b30949366e2f
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-14 17:41:24 -0400

    Delete App.tsx

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import ClientPricingCalculator from './components/ClientPricingCalculator';
-
-export default function App() {
-  return (
-    <div className="p-4">
-      <ClientPricingCalculator />
-    </div>
-  );
-}
