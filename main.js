f83a4b44ca9af6e54e189d6e37e547612426252f
catalogoMACFORT <macprotoolsindustrials@gmail.com>
2025-06-14 00:05:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,1 @@
-function solicitarPin() {
-  alert('Será redirigido a WhatsApp para solicitar su PIN personalizado.');
-  window.open('https://wa.me/59168099278?text=Hola,%20quisiera%20solicitar%20mi%20código%20PIN%20para%20ver%20precios%20del%20catálogo.', '_blank');
-}
+console.log('MACFORT App cargada');
